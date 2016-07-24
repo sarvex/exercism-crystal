d4a5ce358196a520ec2872665e075a2fb3f182f9
Isaac Sloan <isaac@isaacsloan.com>
2016-07-24 00:27:45 -0600

    bracket push added (#27)
    * bracket push added
    
    * crystal formatting

---
+++ exercises/bracket-push/example.cr
@@ -0,0 +1,18 @@
+module Brackets
+  extend self
+  MAP = {'}' => '{', ']' => '[', ')' => '('}
+
+  def are_valid?(str : String)
+    create_stack(filter_brackets(str)).size == 0
+  end
+
+  private def filter_brackets(brackets : String) : Array(Char)
+    brackets.chars.select { |c| (MAP.keys + MAP.values).includes?(c) }
+  end
+
+  private def create_stack(brackets : Array(Char)) : Array(Char)
+    brackets.reduce([] of Char) do |stack, b|
+      MAP[b]? && MAP[b]? == stack.last? ? stack[0..-2] : stack << b
+    end
+  end
+end
