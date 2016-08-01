c693fa81affc2dfbffcbf47e79324078a14d81ca
Isaac Sloan <isaac@isaacsloan.com>
2016-08-01 02:09:28 -0600

    added sieve exercise (#35)
    * added sieve exercise

---
+++ exercises/sieve/example.cr
@@ -0,0 +1,14 @@
+module Primes
+  extend self
+
+  def sieve(limit)
+    sieve_ar = Array.new(limit + 1, true)
+    sieve_ar[0] = sieve_ar[1] = false
+    2.step(Math.sqrt(limit)) do |i|
+      next unless sieve_ar[i]
+      (i*i).step(limit, i) { |ii| sieve_ar[ii] = false }
+    end
+
+    sieve_ar.each_with_index.reduce([] of Int32) { |p, (s, i)| s ? p << i : p }
+  end
+end
