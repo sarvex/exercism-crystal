154ed07ba4f061351eac3664887c8a03f8805ead
Jhonny Moreira <jhonnymoreira@users.noreply.github.com>
2016-11-16 03:50:35 -0200

    [DONE] Standardizing "Hello World" (#51)
    * hello-world: "!" on String at example
    
    * hello-world: "!" on String at spec

--- exercises/hello-world/spec/hello_world_spec.cr
+++ exercises/hello-world/spec/hello_world_spec.cr
@@ -4,15 +4,15 @@ require "../src/*"
 describe "HelloWorld" do
   describe "#hello" do
     it "says hello with default 'World'" do
-      HelloWorld.hello.should eq "Hello, World"
+      HelloWorld.hello.should eq "Hello, World!"
     end
 
     pending "says hello with one name" do
-      HelloWorld.hello("Max").should eq "Hello, Max"
+      HelloWorld.hello("Max").should eq "Hello, Max!"
     end
 
     pending "says hello with another name" do
-      HelloWorld.hello("Alice").should eq "Hello, Alice"
+      HelloWorld.hello("Alice").should eq "Hello, Alice!"
     end
   end
 end
