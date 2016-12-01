64e8546c28329573b668a380b9794464fc905765
Bryan Mulvihill <mulvihill@informz.com>
2016-12-01 08:08:17 -0500

    Use extend self

--- exercises/difference-of-squares/src/example.cr
+++ exercises/difference-of-squares/src/example.cr
@@ -1,13 +1,15 @@
-class Squares
-  def self.square_of_sum(num)
+module Squares
+  extend self
+
+  def square_of_sum(num)
     (num * (num + 1) / 2) ** 2
   end
 
-  def self.sum_of_squares(num)
+  def sum_of_squares(num)
     num * (num + 1) * (2 * num + 1) / 6
   end
 
-  def self.difference_of_squares(num)
+  def difference_of_squares(num)
     square_of_sum(num) - sum_of_squares(num)
   end
 end
