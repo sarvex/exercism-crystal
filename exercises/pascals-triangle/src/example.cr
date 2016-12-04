0eb48b8908c617bb863e732ddaf6b16e7e0f6b90
Bryan Mulvihill <mulvihill@informz.com>
2016-12-03 21:02:22 -0500

    solve with iterator

--- exercises/pascals-triangle/src/example.cr
+++ exercises/pascals-triangle/src/example.cr
@@ -1,12 +1,19 @@
-module PascalsTriangle
-  extend self
+class PascalsTriangle
+  include Iterator(Array(Int32))
 
-  def rows(n : Int32)
-    raise ArgumentError.new("Int must not be less than zero") if n < 0
-    (0..(n - 1)).map do |m|
-      m.times.reduce([1]) do |row, k|
-        row << row[k] * (m - k) / (k + 1)
-      end
+  def self.rows(n)
+    raise ArgumentError.new("Number must not be less than zero") if n < 0
+    new.first(n).to_a
+  end
+
+  def initialize
+    @m = -1
+  end
+
+  def next
+    @m += 1
+    @m.times.reduce([1]) do |row, k|
+      row << (row[k] * (@m - k) / (k + 1))
     end
   end
 end
