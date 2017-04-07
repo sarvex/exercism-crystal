d19f9ce298dc30bbf2de6260ddb046be835eb0f3
Bryan Mulvihill <mulvihill@informz.com>
2017-04-07 15:08:31 -0400

    We need to use named arguments for step

--- exercises/sieve/src/example.cr
+++ exercises/sieve/src/example.cr
@@ -4,9 +4,10 @@ module Primes
   def sieve(limit)
     sieve_ar = Array.new(limit + 1, true)
     sieve_ar[0] = sieve_ar[1] = false
-    2.step(Math.sqrt(limit)) do |i|
+
+    2.step(to: Math.sqrt(limit)) do |i|
       next unless sieve_ar[i]
-      (i*i).step(limit, i) { |ii| sieve_ar[ii] = false }
+      (i*i).step(to: limit, by: i) { |ii| sieve_ar[ii] = false }
     end
 
     sieve_ar.each_with_index.reduce([] of Int32) { |p, (s, i)| s ? p << i : p }
