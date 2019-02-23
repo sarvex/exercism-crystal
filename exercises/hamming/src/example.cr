8e03b85c07402e5affb3383a3b90c67de6190ac3
Anthony Scotti <anthony.m.scotti@gmail.com>
2019-02-23 18:26:50 -0500

    Adds generator for hamming and updates exercise

--- exercises/hamming/src/example.cr
+++ exercises/hamming/src/example.cr
@@ -1,7 +1,7 @@
 class Hamming
-  def self.compute(strand1 : String, strand2 : String)
+  def self.distance(strand1 : String, strand2 : String)
     if strand1.size == strand2.size
-      strand1.chars.zip(strand2.chars).count { |chars| chars[0] != chars[1] }
+      strand1.chars.zip(strand2.chars).count { |first, second| first != second }
     else
       raise ArgumentError.new(message = "Strands must be of equal length")
     end
---
+++ generator/src/generators/hamming.cr
@@ -0,0 +1,52 @@
+require "../exercise_generator"
+require "../exercise_test_case"
+
+class HammingGenerator < ExerciseGenerator
+  def exercise_name
+    "hamming"
+  end
+
+  def test_cases
+    JSON.parse(data)["cases"].as_a.map do |test_case|
+      HammingTestCase.from_json(test_case.to_json)
+    end
+  end
+end
+
+class HammingTestCase < ExerciseTestCase
+  class Input
+    JSON.mapping(
+      strand1: String,
+      strand2: String
+    )
+  end
+
+  class Error
+    JSON.mapping(
+      error: String
+    )
+  end
+
+  JSON.mapping(
+    description: String,
+    property: String,
+    input: Input,
+    expected: Int32 | Error
+  )
+
+  def workload
+    if expected.is_a?(Error)
+      <<-WL
+      expect_raises(ArgumentError) do
+            Hamming.#{property}(\"#{input.strand1}\", \"#{input.strand2}\")
+          end
+      WL
+    else
+      "Hamming.#{property}(\"#{input.strand1}\", \"#{input.strand2}\").should eq(#{expected})"
+    end
+  end
+
+  def test_name
+    description
+  end
+end
