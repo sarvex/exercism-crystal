f136cf242440da26e01a8add4a99de9d84fdad3e
Caleb Miller <caleb@mathmammoth.com>
2020-01-03 10:21:21 -0600

    only add decimal points when not .0

--- exercises/triangle/spec/triangle_spec.cr
+++ exercises/triangle/spec/triangle_spec.cr
@@ -3,19 +3,19 @@ require "../src/*"
 
 describe "Triangle" do
   it "all sides are equal" do
-    Triangle.new({2.0, 2.0, 2.0}).equilateral?.should eq(true)
+    Triangle.new({2, 2, 2}).equilateral?.should eq(true)
   end
 
   pending "any side is unequal" do
-    Triangle.new({2.0, 3.0, 2.0}).equilateral?.should eq(false)
+    Triangle.new({2, 3, 2}).equilateral?.should eq(false)
   end
 
   pending "no sides are equal" do
-    Triangle.new({5.0, 4.0, 6.0}).equilateral?.should eq(false)
+    Triangle.new({5, 4, 6}).equilateral?.should eq(false)
   end
 
   pending "all zero sides is not a triangle" do
-    Triangle.new({0.0, 0.0, 0.0}).equilateral?.should eq(false)
+    Triangle.new({0, 0, 0}).equilateral?.should eq(false)
   end
 
   pending "sides may be floats" do
@@ -23,35 +23,35 @@ describe "Triangle" do
   end
 
   pending "last two sides are equal" do
-    Triangle.new({3.0, 4.0, 4.0}).isosceles?.should eq(true)
+    Triangle.new({3, 4, 4}).isosceles?.should eq(true)
   end
 
   pending "first two sides are equal" do
-    Triangle.new({4.0, 4.0, 3.0}).isosceles?.should eq(true)
+    Triangle.new({4, 4, 3}).isosceles?.should eq(true)
   end
 
   pending "first and last sides are equal" do
-    Triangle.new({4.0, 3.0, 4.0}).isosceles?.should eq(true)
+    Triangle.new({4, 3, 4}).isosceles?.should eq(true)
   end
 
   pending "equilateral triangles are also isosceles" do
-    Triangle.new({4.0, 4.0, 4.0}).isosceles?.should eq(true)
+    Triangle.new({4, 4, 4}).isosceles?.should eq(true)
   end
 
   pending "no sides are equal" do
-    Triangle.new({2.0, 3.0, 4.0}).isosceles?.should eq(false)
+    Triangle.new({2, 3, 4}).isosceles?.should eq(false)
   end
 
   pending "first triangle inequality violation" do
-    Triangle.new({1.0, 1.0, 3.0}).isosceles?.should eq(false)
+    Triangle.new({1, 1, 3}).isosceles?.should eq(false)
   end
 
   pending "second triangle inequality violation" do
-    Triangle.new({1.0, 3.0, 1.0}).isosceles?.should eq(false)
+    Triangle.new({1, 3, 1}).isosceles?.should eq(false)
   end
 
   pending "third triangle inequality violation" do
-    Triangle.new({3.0, 1.0, 1.0}).isosceles?.should eq(false)
+    Triangle.new({3, 1, 1}).isosceles?.should eq(false)
   end
 
   pending "sides may be floats" do
@@ -59,19 +59,19 @@ describe "Triangle" do
   end
 
   pending "no sides are equal" do
-    Triangle.new({5.0, 4.0, 6.0}).scalene?.should eq(true)
+    Triangle.new({5, 4, 6}).scalene?.should eq(true)
   end
 
   pending "all sides are equal" do
-    Triangle.new({4.0, 4.0, 4.0}).scalene?.should eq(false)
+    Triangle.new({4, 4, 4}).scalene?.should eq(false)
   end
 
   pending "two sides are equal" do
-    Triangle.new({4.0, 4.0, 3.0}).scalene?.should eq(false)
+    Triangle.new({4, 4, 3}).scalene?.should eq(false)
   end
 
   pending "may not violate triangle inequality" do
-    Triangle.new({7.0, 3.0, 2.0}).scalene?.should eq(false)
+    Triangle.new({7, 3, 2}).scalene?.should eq(false)
   end
 
   pending "sides may be floats" do
